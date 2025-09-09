6015b89d1d0fe4fc355ce62af3c5a15f79416ebd
Bruno Serra <bbucalonserra@hotmail.com>
2025-09-09 08:55:12 -0300

    Standardizing comments in script.

--- final/project/script.js
+++ final/project/script.js
@@ -1,6 +1,7 @@
-// THE SCRIPT WILL RUNS WHEN THE BROWSER FINDS THE SCRIPT TAG IN THE HTML. HOWEVER WE ADDED A DEFER, THEREFORE IT WILL RUN AFTER THE HTML IS ALREADY LOADED (AFTER HANDDLEBAR). 
+// THE SCRIPT WILL RUN WHEN THE BROWSER FINDS THE SCRIPT TAG IN THE HTML. 
+// HOWEVER WE ADDED A DEFER, THEREFORE IT WILL RUN AFTER THE HTML IS ALREADY LOADED (AFTER HANDLEBARS). 
 
-// --------------------------- Change slider color --------------------------- 
+// ------------------------------- Change slider color --------------------------------
 function main_anchors() {
     // Selects all slider dots inside <main>
     document.querySelectorAll("main .slider-dot").forEach(anchor => {
@@ -22,35 +23,36 @@ function main_anchors() {
         });
     });
 }
-// --------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
 
-// --------------------------- Changeing colors using hover in circles. ---------------------------  
 
+// ------------------------- Changing colors using hover in circles -------------------
 function hoverCards() {
-    // Create a variable to store all the ids from circles. Return a HTML collection length 4. Because the class has 4 anchors (<a>).
+    // Create a variable to store all the ids from circles. Return a HTML collection length 4.
     let hover_total = document.getElementsByClassName("circle");
 
     // Loop for all the items inside variable circles, avoiding create a single code structure for each circle.
     for (let i = 0; i < hover_total.length; i++) {
 
-        // First, we store the element_id in the variable circle (example, loop 1, let circle = circle_1)
-        // NOTE: document is the HTM itself.
+        // First, we store the element_id in the variable circle (example: circle_1, circle_2, etc.)
         let circle = document.getElementById("circle_" + (i + 1));
 
-        // Now, we add an event listner for mouseover, hence when the user mouse over the tag with id = circle_1 (1,2,3,4), change background color and font color. 
+        // Now, we add an event listener for mouseover
         circle.addEventListener("mouseover", function() {
             circle.style.backgroundColor = "#3c9242";
             circle.style.color = "white";
         });
 
-        // Finally,  we add an event listner for mouse out, hence when the user mouse out the tag with id = circle_1 (1,2,3,4), change back again.
-        // We'll use remove to remove attribute.
+        // Event listener for mouseout
         circle.addEventListener("mouseout", function() {
             circle.removeAttribute("style");
         });
     }
 }
-// --------------------------- Back to the top button --------------------------- 
+// ------------------------------------------------------------------------------------
+
+
+// ----------------------------- Back to the top button -------------------------------
 function backToTheTop () {
     let top = document.getElementById("button-back-to-the-top");
 
@@ -59,40 +61,40 @@ function backToTheTop () {
         window.scrollTo({top: 0, behavior: "smooth" });
     })
 };
+// ------------------------------------------------------------------------------------
 
 
-// --------------------------- Tracking events using JavaScript ---------------------------
-// --------------------------- Homepage ---------------------------
-// ----- Track clicks from slider banner 
+// -------------------------- Tracking events using JavaScript ------------------------
+ 
+// ------------------------------- Homepage: slider clicks ----------------------------
 function eventSliderBannerClicked () {
-
     let dot_total = document.getElementsByClassName("slider-dot");
 
     for (let i = 0; i < dot_total.length; i++) {
-
         let hover = document.getElementById("dot-" + i);
-        
         hover.addEventListener("click", function () {
-            console.log("Event Slider Bannder Clicked: slider number " + i);
+            console.log("Event Slider Banner Clicked: slider number " + i);
         })  
     }
 }
+// ------------------------------------------------------------------------------------
 
-// ----- Track interactions with hover from circles
+
+// ----------------------- Homepage: interactions with circles ------------------------
 function eventHoverInOut () {
-
     let hover_total = document.getElementsByClassName("circle")
 
     for (let i = 0; i < hover_total.length; i++) {
-
         let hover = document.getElementById("circle_" + (i + 1));
         hover.addEventListener("mouseover", function () {
             console.log("Event Hover in Card: card number " + i);
         })  
     }
 }
+// ------------------------------------------------------------------------------------
 
-// ----- Track button back to the top clicked
+
+// --------------------- Homepage: back to the top button click -----------------------
 function EventBackToTheTop () {
     let top = document.getElementById("button-back-to-the-top");
 
@@ -100,19 +102,22 @@ function EventBackToTheTop () {
         console.log("Event Back to the Top Button Clicked")
     })
 };
+// ------------------------------------------------------------------------------------
 
-// -- Function to track scroll.
+
+// ------------------------------- Homepage: track scroll -----------------------------
 function EventScroll () {
     window.addEventListener("scroll", function() {
-
-    // window.innerHeight = visible screen of the browser.
-    // window.scrollY = how much the user scrolled down.
-    // Hence, window.innerHeight + window.scroll = how much users saw the page.
-    // document.documentElement.scrollHeight = total page content
+        // window.innerHeight = visible screen of the browser.
+        // window.scrollY = how much the user scrolled down.
+        // Hence, window.innerHeight + window.scrollY = how much users saw the page.
+        // document.documentElement.scrollHeight = total page content
     
-    let page_size = document.documentElement.scrollHeight;
+        let page_size = document.documentElement.scrollHeight;
 
-    if (window.innerHeight + window.scrollY >= page_size) {
-        console.log("Event Scroll Until End of Page (page size = " + page_size + "px)");
-    }})
+        if (window.innerHeight + window.scrollY >= page_size) {
+            console.log("Event Scroll Until End of Page (page size = " + page_size + "px)");
+        }
+    })
 }
+// ------------------------------------------------------------------------------------
